4f082a6ff0d167d05a632461c86ee6e87b828628
Aswin-S-T <aswinswebart15@gmail.com>
2022-06-06 12:02:03 +0530

    awork

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const dbUrl = 'mongodb+srv://aswins:7VtfydtNGvOiO6o7@cluster0.ptzkj.mongodb.net/?retryWrites=true&w=majority'
 // const dbUrl = 'mongodb://localhost:27017/usersblog'
-// console.log('DB URL===============',dbUrl)
+console.log('DB URL===============',dbUrl)
 module.exports.connect = function(done){
     mongoose.connect(dbUrl,{
         useNewUrlParser:true,
