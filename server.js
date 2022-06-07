6fbcdd1c15dbc0b6f4b7fed0145327e1fd3be978
Aswin-S-T <aswinswebart15@gmail.com>
2022-06-07 15:20:47 +0530

    dsd

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ db.connect()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // console.log('PATH=========>',path.join(__dirname,'../frontend/uploads')) // path.join(__dirname+'../test/karma.conf.js')
 
@@ -28,5 +28,6 @@ app.use('/api/v1/user',userRouter)
 app.use('/api/v1/uploads',uploadRouter)
 
 app.listen(port,()=>{
+    console.log('DIR NMAE : ',__dirname)
     console.log(`Server is running at the port ${port}`)
 })
